2052385fcbb989f05a21d3cea644da251fd951f4
step.twig@gmail.com <step.twig@gmail.com>
2022-09-27 14:18:02 +0000

    add database seeder functions

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\User;
+use App\Customer;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +13,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+        User::truncate();
+        Customer::truncate();
+
         // $this->call(UsersTableSeeder::class);
+        // User::factory(3)->create();
+        User::create([
+            'name' => "Bobby",
+            'email' => "Bobby",
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+
+        Customer::create([
+            'contact_number' => '05400113224',
+            'status_code' => 1,
+            'is_whitelisted' => 0
+        ]);
+
+        Customer::create([
+            'contact_number' => '05400113224',
+            'status_code' => 1,
+            'is_whitelisted' => 0
+        ]);
+        
+
     }
 }
